9688374231dcf3324d0c18fa967d71d8b8d11a48
Aleksander Glowacki <a.glowacki@student.pk.edu.pl>
2023-12-18 20:27:39 +0100

    Working listing

--- project/app/main.py
+++ project/app/main.py
@@ -1,16 +1,34 @@
 from views.view import ProductView
-# import tkinter as tk
 from controllers.productController import ProductController
-
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+controller = ProductController()
 
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-controller = ProductController()
+
+@app.route('/list')
+def list():
+    controller.get("", get_one="False")
+    return render_template('list.html', products=controller.product_list)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
 # view = ProductView(app, controller)
 
 if __name__ == '__main__':
